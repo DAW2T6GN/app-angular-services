f200f7bff1afedff925c3f3341494acb4a1a5507
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2023-06-17 20:12:09 -0500

    agregando angular routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './dashboard/home/home.component';
 import { CursoListaComponent } from './dashboard/curso/curso-lista/curso-lista.component';
 import { ProductoListaComponent } from './dashboard/producto/producto-lista/producto-lista.component';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './angular-material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -20,7 +24,21 @@ import { ProductoListaComponent } from './dashboard/producto/producto-lista/prod
     ProductoListaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent},
+      {path: 'dashboard', component: DashboardComponent,
+        children: [
+          {path: 'home', component: HomeComponent},
+          {path: 'curso', component: CursoListaComponent},
+          {path: 'producto', component: ProductoListaComponent}
+        ]
+      },
+      {path: '', redirectTo: 'login', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]

f4e58aa8398d0ec4a0466f9da4fc893746f7ed4b
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2023-06-17 19:58:10 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './dashboard/home/home.component';
+import { CursoListaComponent } from './dashboard/curso/curso-lista/curso-lista.component';
+import { ProductoListaComponent } from './dashboard/producto/producto-lista/producto-lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+    DashboardComponent,
+    HomeComponent,
+    CursoListaComponent,
+    ProductoListaComponent
   ],
   imports: [
     BrowserModule

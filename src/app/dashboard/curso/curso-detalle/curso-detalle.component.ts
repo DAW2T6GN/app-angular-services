b79a5d2e0d120e573d0187bd7530601a3c5d8d05
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2023-06-24 20:17:59 -0500

    Obtener Url Parametro .

--- src/app/dashboard/curso/curso-detalle/curso-detalle.component.ts
+++ src/app/dashboard/curso/curso-detalle/curso-detalle.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-curso-detalle',
   templateUrl: './curso-detalle.component.html',
   styleUrls: ['./curso-detalle.component.scss']
 })
-export class CursoDetalleComponent {
+export class CursoDetalleComponent implements OnInit {
+
+  cursoId: string | null = "";
+
+  constructor(private router: ActivatedRoute){
+
+  }
+
+  ngOnInit(): void {
+    this.cursoId = this.router.snapshot.paramMap.get('id');
+  }
+
 
 }
